9b158a6c732e040fda7c8a57f3126ec92b2fdab7
jobscale <jobscale@users.noreply.github.com>
2023-04-07 21:01:55 +0900

    wait updated

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { fetch } = require('@jobscale/fetch');
 const { kabuka } = require('./app');
 const { list } = require('./app/list');
 
+const wait = ms => new Promise(resolve => { setTimeout(resolve, ms); });
+
 class App {
   postSlack(data) {
     const url = 'https://tanpo.jsx.jp/api/slack';
@@ -31,20 +33,11 @@ class App {
     });
   }
 
-  wait(ms) {
-    const prom = {};
-    prom.pending = new Promise((...argv) => { [prom.resolve, prom.reject] = argv; });
-    setTimeout(prom.resolve, ms);
-    return prom.pending;
-  }
-
   async start() {
-    // eslint-disable-next-line no-restricted-syntax
     for (let i = 0; i < list.length;) {
       const code = list[i];
       await this.execute(code);
-      // eslint-disable-next-line no-plusplus
-      if (++i < list.length) await this.wait(5000);
+      if (++i < list.length) await wait(10000); // eslint-disable-line no-plusplus
     }
   }
 }
