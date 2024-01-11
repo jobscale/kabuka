edd7eb5441f168058fd36e2449d82dc34dd3449b
jobscale <jobscale@users.noreply.github.com>
2024-01-11 18:45:58 +0900

    get rate updated

--- app/index.js
+++ app/index.js
@@ -1,16 +1,17 @@
+const { logger } = require('@jobscale/logger');
 const { JSDOM } = require('jsdom');
 
 const url = 'https://finance.yahoo.co.jp/quote/{{code}}';
 
 class Kabuka {
   scraping(document) {
-    const main = document.querySelector('#all_rate').parentNode;
+    const main = document.querySelector('#detail').parentNode;
     const header = main.querySelector('header');
     const section = header.parentElement;
     const value = section.querySelector('div:nth-child(3) > div:nth-child(2)').textContent;
     const name = header.querySelector('div:nth-child(1)').textContent;
     const price = header.querySelector('div:nth-child(2)').textContent;
-    const rate = main.querySelector('#all_rate > div > div > div:nth-child(2) > span > span').textContent;
+    const rate = (main.querySelector('#all_rate > div > div > div:nth-child(2) > span > span') || {}).textContent;
     return { value, price, name, rate };
   }
 
@@ -24,7 +25,8 @@ class Kabuka {
     .then(res => {
       const { value, price, name, rate } = res;
       return `${value}  |  ${price}\n${rate}  |  <${uri}|${name}  ${code}>`;
-    });
+    })
+    .catch(e => logger.warn({ code }, e));
   }
 }
 
